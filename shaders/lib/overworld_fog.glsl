fcb5d79acf8044ccf0e572f39a63358108477f41
Merlin1809 <lucas.kamionka@gmail.com>
2025-04-09 19:36:18 +0200

    Readded rain fog

--- shaders/lib/overworld_fog.glsl
+++ shaders/lib/overworld_fog.glsl
@@ -33,7 +33,7 @@ float cloudVol(in vec3 pos, float maxDistance ){
 	}
 	
 	float cloudyFog = max(min(max(fog_shape - 0.6 ,0.0) * 2.0 ,1.0) - fog_erosion * 0.4	, 0.0)	*	exp(-0.05 * max(pos.y - (fogYstart+20),0.0));
-	float rainyFog = (low_gradientFog * 0.5 + exp2(-0.06 * max(pos.y - fogYstart,0.0))) * rainStrength * noPuddleAreas;
+	float rainyFog = (exp2(-0.008 * max(pos.y - fogYstart-500,0.0)) * 0.01 + high_gradientFog) * rainStrength * noPuddleAreas;
 	
 	if(sandStorm > 0.0 || snowStorm > 0.0){
 		float IntenseFogs = pow(1.0 - densityAtPosFog( (samplePos2  - vec3(frameTimeCounter,0,frameTimeCounter)*15.0) * 100.0),2.0) * mix(1.0, high_gradientFog, snowStorm);
@@ -44,7 +44,7 @@ float cloudVol(in vec3 pos, float maxDistance ){
 
 	FogDensities(medium_gradientFog, cloudyFog, rainyFog, maxDistance, 1.0, 1.0);
 
-	return uniformFog + medium_gradientFog + cloudyFog;
+	return uniformFog + medium_gradientFog + cloudyFog + rainyFog;
 }
 
 float phaseRayleigh(float cosTheta) {

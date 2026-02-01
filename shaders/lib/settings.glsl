4caedb64ab81239578e4f34620c37ccfa7a5d152
Merlin1809 <88194861+Merlin1809@users.noreply.github.com>
2026-02-01 19:32:08 +0100

    Fix SSS type not set to auto

--- shaders/lib/settings.glsl
+++ shaders/lib/settings.glsl
@@ -335,7 +335,7 @@ const float entityShadowDistanceMul = 0.25; // [0.01 0.02 0.03 0.04 0.05 0.10 0.
 #ifdef DEPTH_WRITE_POM
 #endif
 
-#define SSS_TYPE_SETTING 1 // [0 1 2 3 4]
+#define SSS_TYPE_SETTING 4 // [0 1 2 3 4]
 #define LabSSS_Curve 1.0 // [0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9 3.0]
 // #define MOB_SSS
 // #define MISC_BLOCK_SSS
